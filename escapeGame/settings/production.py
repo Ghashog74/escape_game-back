0130acb8412ab14ddf25e6a85e03cee78eab47e7
Quentin Montreuil <quentinmontreuil06@orange.fr>
2025-04-11 08:59:24 +0200

    fix production config file

--- escapeGame/settings/production.py
+++ escapeGame/settings/production.py
@@ -46,6 +46,14 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt'
 ]
 
+ASGI_APPLICATION = 'escapeGame.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default':{
+        'BACKEND':'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
